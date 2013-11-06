c8b270ee2f080e809531ea6a802972194be1f821
Laurence Roberts <lsjroberts@outlook.com>
2013-11-06 23:22:25 +0000

    Added base conversational and emotional actor classes

--- app/actor.py
+++ app/actor.py
@@ -42,3 +42,29 @@ class MoveableActor( Actor ):
 # An actor that can be positioned by the player.
 class ControllableActor( MoveableActor ):
 	pass
+
+
+# ----------- Conversational Actor -----------
+# An actor who can hold a conversation.
+class ConversationalActor( Actor ):
+	pass
+
+
+# ----------- Emotional Actor -----------
+# An actor who has emotions and reactions.
+class EmotionalActor( Actor ):
+	
+	def addEmotion( emotion ):
+		if self.emotions is None:
+			self.emotions = {}
+
+		self.emotions[emotion.name] = emotion
+
+	def addTargetedEmotion( emotion, target ):
+		if self.targetedEmotions is None:
+			self.targetedEmotions = {}
+
+		if self.targetedEmotions[target.name] is None:
+			self.targetedEmotions[target.name] = {}
+
+		self.targetedEmotions[target.name][emotion.name] = emotion
