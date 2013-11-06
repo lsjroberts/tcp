019a2279d1c5ca4a2c33552749c441db032271c1
Laurence Roberts <lsjroberts@gmail.com>
2013-11-06 12:58:46 +0000

    Fixed some more indentation

--- app/sprite.py
+++ app/sprite.py
@@ -17,8 +17,8 @@ class Sprite( pygame.sprite.Sprite ):
     #
     # @return Sprite
     def __init__( self ):
-    	self.groups = self.groups or []
-    	pygame.sprite.Sprite.__init__( self, self.groups )
+        self.groups = self.groups or []
+        pygame.sprite.Sprite.__init__( self, self.groups )
 
 
 # -------- Static Sprite --------
@@ -30,21 +30,21 @@ class StaticSprite( Sprite ):
     #
     # @return StaticSprite
     def __init__( self, src, vector ):
-    	Sprite.__init__( self )
+        Sprite.__init__( self )
 
-    	self.vector = vector
-    	self.image = pygame.image.load( config.folders['assets'] + src ).convert_alpha( )
-    	self.rect = self.image.get_rect( )
-    	self.rect.x = self.vector.x
-    	self.rect.y = self.vector.y
+        self.vector = vector
+        self.image = pygame.image.load( config.folders['assets'] + src ).convert_alpha( )
+        self.rect = self.image.get_rect( )
+        self.rect.x = self.vector.x
+        self.rect.y = self.vector.y
 
     # -------- Update --------
     # Update the sprite's position
     #
     # @return StaticSprite
     def update( self, frameTime, lifeTime ):
-    	self.rect.x = self.vector.x
-    	self.rect.y = self.vector.y
+        self.rect.x = self.vector.x
+        self.rect.y = self.vector.y
 
 
 # -------- Animated Sprite --------
@@ -56,14 +56,14 @@ class AnimatedSprite( Sprite ):
     #
     # @return AnimatedSprite
     def __init__( self, src, vector ):
-    	Sprite.__init__( self )
+        Sprite.__init__( self )
 
-    	self.frames = [ ]
-    	self.numFrames = 0
-    	self.states = { }
-    	self.vector = vector
+        self.frames = [ ]
+        self.numFrames = 0
+        self.states = { }
+        self.vector = vector
 
-    	self.srcImage = pygame.image.load( config.folders['assets'] + src ).convert_alpha( )
+        self.srcImage = pygame.image.load( config.folders['assets'] + src ).convert_alpha( )
         self.srcWidth, self.srcHeight = self.srcImage.get_size( )
 
         self.lastUpdate = 0
