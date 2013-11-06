0c01ed32c5b2b531deea2558b05ddc4ec3f8498e
Laurence Roberts <lsjroberts@gmail.com>
2013-11-06 01:17:31 +0000

    Added app class

---
+++ app/app.py
@@ -0,0 +1,78 @@
+# ----------- App ------------
+# Handles all general application logic
+# ----------------------------
+
+
+# -------- Imports --------
+
+import pygame, config
+from event import EventManager, EventListener
+
+
+# -------- App --------
+# Global app class.
+class App( ):
+
+    # -------- Init --------
+    # Constructor, creates the app and sets it to running.
+    #
+    # @return App
+    def __init__( self ):
+        # Set the app to running
+        self.running = True
+
+        # Create the event manager
+        self.events = EventManager( )
+        self.events.registerListener( AppListener() )
+
+        # Set the app mode
+        self.mode = 'game'
+
+    # -------- Tick --------
+    # Process a single tick of the game loop.
+    #
+    # @param  int  frameTime Number of milliseconds passed since the previous
+    #              tick.
+    # @param  int  lifeTime  Number of milliseconds since pygame initialised.
+    # @return void
+    def tick( self, frameTime, lifeTime ):
+        if 'game' == self.mode:
+            self.tickGame( frameTime, lifeTime )
+        else:
+            self.tickMenu( frameTime, lifeTime )
+
+    # -------- Tick Game --------
+    # Process a single tick within the game mode.
+
+    # @param  int  frameTime Number of milliseconds passed since the previous
+    #              tick.
+    # @param  int  lifeTime  Number of milliseconds since pygame initialised.
+    # @return void
+    def tickGame( self, frametime, lifeTime ):
+        pass
+
+    # -------- Tick Menu --------
+    # Process a single tick within the menu mode.
+
+    # @param  int  frameTime Number of milliseconds passed since the previous
+    #              tick.
+    # @param  int  lifeTime  Number of milliseconds since pygame initialised.
+    # @return void
+    def tickMenu( self, frametime, lifeTime ):
+        pass
+
+
+# -------- App Listener --------
+# Listen for and handle app events.
+class AppListener( EventListener ):
+
+    # -------- Notify --------
+    # Listen for and handle an event.
+    #
+    # @param  Event event
+    # @return void
+    def notify( self, event ):
+        if 'pygame' == event.name:
+            if pygame.QUIT == event.data.type:
+                config.app.running = False
+                print 'Exiting app...'
