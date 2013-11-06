962db1fc765e9f983f5df543b275b22e8b4859d6
Laurence Roberts <lsjroberts@gmail.com>
2013-11-06 12:57:13 +0000

    Added initial actors

---
+++ app/actor.py
@@ -0,0 +1,20 @@
+# ----------- Actor ------------
+# Actors represent interactable characters within the game world.
+# ----------------------------
+
+
+# -------- Imports --------
+
+import config
+
+
+# -------- Actor --------
+# Base abstract actor
+class Actor( ):
+
+    # -------- Init --------
+    # Constructor
+    #
+    # @return Actor
+    def __init__( self ):
+    	pass
