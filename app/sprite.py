16c20b16d4e4fe91237452b040effc025530f40e
Laurence Roberts <lsjroberts@outlook.com>
2013-11-06 21:01:04 +0000

    Updated sprites

--- app/sprite.py
+++ app/sprite.py
@@ -33,7 +33,7 @@ class StaticSprite( Sprite ):
         Sprite.__init__( self )
 
         self.vector = vector
-        self.image = pygame.image.load( config.folders['assets'] + src ).convert_alpha( )
+        self.image = pygame.image.load( config.folders['sprites'] + src ).convert_alpha( )
         self.rect = self.image.get_rect( )
         self.rect.x = self.vector.x
         self.rect.y = self.vector.y
@@ -41,6 +41,8 @@ class StaticSprite( Sprite ):
     # -------- Update --------
     # Update the sprite's position
     #
+    # @param  int  frameTime Number of milliseconds passed since the previous tick.
+    # @param  int  lifeTime  Number of milliseconds since pygame initialised.
     # @return StaticSprite
     def update( self, frameTime, lifeTime ):
         self.rect.x = self.vector.x
@@ -58,14 +60,90 @@ class AnimatedSprite( Sprite ):
     def __init__( self, src, vector ):
         Sprite.__init__( self )
 
-        self.frames = [ ]
+        self.images = []
         self.numFrames = 0
-        self.states = { }
+        self.states = {}
         self.vector = vector
+        self.loaded = False
 
-        self.srcImage = pygame.image.load( config.folders['assets'] + src ).convert_alpha( )
+        self.srcImage = pygame.image.load( config.folders['sprites'] + src ).convert_alpha( )
         self.srcWidth, self.srcHeight = self.srcImage.get_size( )
 
         self.lastUpdate = 0
         self.frame = 0
         self.state = ''
+
+    # ----------- Add Animation State -----------
+    # 
+    # @param  string name  State identifier name
+    # @param  int    start Frame to start the animation at
+    # @param  int    end   Frame to end the animation at
+    # @param  int    fps   Number of frames to show per second
+    # @return None
+    def addAnimationState( self, name, start, end, fps ):
+        self.numFrames += ( end - start + 1 )
+        self.states[name] = {
+            'start': start,
+            'end': end,
+            'delay': ( 1000 / fps )
+        }
+
+    # ----------- Load States -----------
+    # Split the src image into each frame image.
+    # 
+    # @return None
+    def loadStates( self ):
+        self.frameWidth = self.srcWidth / self.numFrames
+        for i in range( self.numFrames ):
+            subSurface = self.srcImage.subsurface( i * self.frameWidth, 0, self.frameWidth, self.srcHeight )
+            self.images.append( subSurface )
+        self.image = self.images[0]
+        self.rect = self.image.get_rect( )
+        self.rect.x = self.vector.x
+        self.rect.y = self.vector.y
+
+    # ----------- Set Animation State -----------
+    # 
+    # @param  string name State identifier name.
+    # @return None
+    def setAnimationState( self, name ):
+        if name != self.state:
+            if False == self.loaded:
+                self.loadStates( )
+            self.state = name
+            self.frame = self.states[name]['start']
+            self.lastUpdate = 0
+
+    # ----------- Update Animation -----------
+    # 
+    # @param  int lifeTime Number of milliseconds since pygame initialised.
+    # @return None
+    def updateAnimation( self, lifeTime ):
+        # Get the current state
+        state = self.states[self.state]
+
+        # Update the rect vector position
+        self.rect.x = self.vector.x
+        self.rect.y = self.vector.y
+
+        # Check if enough time has passed since the last update for this state
+        if lifeTime - self.lastUpdate > state['delay']:
+            self.frame += 1
+
+            # Wrap the frame between the state start and end frame
+            if self.frame < state['start']: self.frame = state['start']
+            if self.frame > state['end']: self.frame = state['end']
+
+            # Change the sprite image to that of the current frame
+            self.image = self.images[self.frame]
+
+            # Store the update time
+            self.lastUpdate = lifeTime
+
+    # ----------- Update -----------
+    # 
+    # @param  int  frameTime Number of milliseconds passed since the previous tick.
+    # @param  int  lifeTime  Number of milliseconds since pygame initialised.
+    # @return None
+    def update( self, frameTime, lifeTime ):
+        self.updateAnimation( lifeTime )
