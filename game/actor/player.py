ee3f213e16461990f4c234d3227bdc7eee54701e
Laurence Roberts <lsjroberts@outlook.com>
2013-11-06 23:23:31 +0000

    Added initial player actors

---
+++ game/actor/player.py
@@ -0,0 +1,27 @@
+# ----------- Player ----------
+# Player controllable actors
+# -----------------------------
+
+
+# -------- Imports --------
+
+import config
+from app.player import Player
+
+
+# ----------- Hacker -----------
+# 
+class Hacker( Player ):
+	pass
+
+
+# ----------- GSB -----------
+# 
+class Founder( Player ):
+	pass
+
+
+# ----------- Android -----------
+#
+class Android( Player ):
+	pass
